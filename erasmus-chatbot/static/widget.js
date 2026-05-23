e1ed4e027751bff1306c8bd1208ea893039d0c2d
Christian-alt-star <jchristian.gonzalez@alumnos.colegiosma.com>
2026-05-23 15:51:12 +0200

    Update widget.js

--- erasmus-chatbot/static/widget.js
+++ erasmus-chatbot/static/widget.js
@@ -26,7 +26,7 @@
 
     try {
       // Petición directa sin forzar parámetros estrictos
-      const res = await fetch("https://https://chatbot-eygx.onrender.com://onrender.com", {
+      const res = await fetch("https://chatbot-eygx.onrender.com/chat", {
         method: "POST",
         headers: {
           "Content-Type": "application/json"
